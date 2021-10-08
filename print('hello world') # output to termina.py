5095e41df24dc6ef7bd595e21cbc2930eeca9794
Yulin2004 <yroozemond@gmail.com>
2021-10-08 09:25:50 +0200

    eerste commit

---
+++ print('hello world') # output to termina.py
@@ -0,0 +1,31 @@
+print('hello world') # output to terminal
+name = input('what s your name') # input from terminal
+print ('hello,' + name)  # input from terminal
+input('how old are you')
+input ('what is your favorite game' + name)
+print ('nice choice')
+since = 4.000000
+up = 4
+
+result = up + since 
+
+print(result)
+
+print(str(result)
+
+hello wereld van                                    
+
+    pppp    y     y   ttttttt   h   h    oooo   nnnn          
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n
+
+ print (ýulin roozemond 99067028) # output to terminal
+
+
+hello ((pppp(str)'hallo')
+
+
